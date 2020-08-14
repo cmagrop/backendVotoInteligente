8b61af19be2faddf23269a0286168b8a6679e366
Christian Magro <cmagrop@gmail.com>
2020-08-14 12:38:38 -0400

    Validaciones y Registro de voto

--- application/controllers/Votacion.php
+++ application/controllers/Votacion.php
@@ -18,11 +18,7 @@ class Votacion extends REST_Controller
         $this->load->database();
 
     } 
-    public function index()
-    {
-        echo "Hola mundo";
-    
-    }
+   
 
 //listar votaciones disponibles
 
@@ -36,6 +32,87 @@ public function index_get()
     echo json_encode($query->result());
 
     
+}
+
+
+public function index_post()
+{
+
+    $data=$this->post();//llena un arreglo con los datos de entrada
+    //validacion del rut y el password
+    if(!isset($data['rut']) OR !isset($data['nro_votacion']))
+    {
+    
+        $respuesta= array(
+             'error'=>TRUE,
+             'mensaje'=>'La información enviada no es válida'
+
+
+        );
+
+        $this->response($respuesta,REST_Controller::HTTP_BAD_REQUEST);
+      return;
+    }
+
+    $condiciones= array(
+
+        'FK_rut'=>$data['rut'],
+        'FK_nro_votacion'=>$data['nro_votacion']
+    );
+
+  
+    //realiza consulta a la base de datos
+    $query = $this->db->get_where('usuario_votacion',$condiciones);
+    /*
+       select *
+       from usuario_votacion
+       where FK_rut=$data['rut'] and FK_nro_votacion=$data['nro_votacion'];
+
+    */
+    
+    $votaciones= $query->row();
+
+    if(!isset($votaciones))
+    {
+          $respuesta= array(
+            'error'=>TRUE,
+            'mensaje'=>'Su voto ha sido registrado'
+
+          );
+
+         //realiza 
+         $query = $this->db->insert('usuario_votacion',
+    array('FK_rut' => $data['rut'],
+    'FK_nro_votacion'=>$data['nro_votacion']));
+
+          $this->response($respuesta);
+          return;
+         
+    }
+
+    else
+    {
+        $respuesta= array(
+            'error'=>false,
+            'mensaje'=>'Usted realizó la votación, por lo tanto no puede votar '
+
+          );
+
+
+          $this->response($respuesta);
+          return;
+
+
+    }
+
+   
+   
+
+   
+
+    
+
+
 }
 
 //listar votacionas pasadas limite 6
